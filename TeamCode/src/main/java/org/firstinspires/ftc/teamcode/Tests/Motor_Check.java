581c5a119e442688daafd835812222f288304ab3
Ben <benghent@gmail.com>
2021-03-22 04:10:06 -0500

    Calibrate motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/Motor_Check.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+
+@Autonomous(name = "Motor_Check", group = "Test")
+//@Disabled
+public class Motor_Check extends LinearOpMode {
+	
+	Hardware r = new Hardware();
+	
+	int initial, end;
+	DcMotor[] motors;
+	
+	@Override
+	public void runOpMode() {
+		r.initRobot(hardwareMap, telemetry);
+		r.initAutonomous();
+		
+		motors = new DcMotor[]{r.frontLeft, r.frontRight, r.backLeft, r.backRight, r.Flywheel, r.Launcher, r.Intake};
+		
+		telemetry.setAutoClear(false);
+		
+		telemetry.addData("Pre-Check", "Commencing Pre-Check");
+		telemetry.update();
+		
+		for (DcMotor dcMotor : motors) {
+			dcMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+			dcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		}
+		
+		telemetry.addData("Pre-Check", "Pre-Check Complete");
+		telemetry.addData("Waiting", " to start");
+		telemetry.update();
+		
+		waitForStart();
+		
+		telemetry.addData("Starting", "Yay!");
+		telemetry.update();
+		
+		// This class was specifically developed to test all of our motors
+		// before a match, this insures that our motors are not burnt out
+		// or not working before we require it
+		
+		// To start we are going to test our driving wheels
+		// actually.. lets just do everything in one loop
+		
+		for (DcMotor motor : motors) {
+			initial = motor.getCurrentPosition();
+			motor.setPower(0.6);
+			r.waiter(500);
+			end = motor.getCurrentPosition();
+			motor.setPower(0);
+			telemetry.addLine()
+					.addData("inital", initial)
+					.addData("ending", end)
+					.addData("Error?", initial == end);
+			telemetry.update();
+		}
+		
+		// Continue displaying the telemetry
+		while (!isStopRequested());
+		
+	}
+}
